1f7d241e39ad2a3417c5636b3dd1571b4b4db3f3
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-09-03 11:13:44 +0700

    Create Phần tử riêng biệt.java

---
+++ Phần tử riêng biệt.java
@@ -0,0 +1,16 @@
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.*;
+public class Main {
+    private static int mod = 1000000007;
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        Set<Integer> se = new HashSet<>();
+        for(int i = 0; i < n ;i++){
+            int x = sc.nextInt();
+            se.add(x);
+        }
+        System.out.print(se.size());
+    }
+}
