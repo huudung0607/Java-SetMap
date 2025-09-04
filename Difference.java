f0a389164def58f63a2e283c751c2f8343084196
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-09-04 08:38:26 +0700

    Create Difference.java

---
+++ Difference.java
@@ -0,0 +1,31 @@
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.*;
+public class Main {
+    private static int mod = 1000000007;
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = sc.nextInt(), m = sc.nextInt();
+        int []a = new int[n];
+        HashMap<Integer,Integer> mp = new HashMap<>();
+        for(int i = 0;i < n;i++){
+            a[i] = sc.nextInt();
+            mp.put(a[i],1);
+        }
+        for(int i = 0; i < m;i++){
+            int x = sc.nextInt();
+            if(mp.containsKey(x) && mp.get(x) == 1){
+                mp.put(x,2);
+            }
+        }
+        TreeSet<Integer> se = new TreeSet<>();
+        for(int i = 0; i < n;i++){
+            if(mp.get(a[i]) != 2){
+                se.add(a[i]);
+            }
+        }
+        for(int i : se){
+            System.out.print(i + " ");
+        }
+    }
+}
