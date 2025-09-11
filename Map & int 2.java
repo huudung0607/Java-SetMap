e87102dcf1af3911ff34e8a1a1917e95173326de
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-09-11 14:43:13 +0700

    Create Map & int 2.java

---
+++ Map & int 2.java
@@ -0,0 +1,18 @@
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.*;
+public class Main {
+    private static int mod = 1000000007;
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        LinkedHashMap<Integer,Integer> mp = new LinkedHashMap<>();
+        for(int i = 0;i < n;i++){
+            int x = sc.nextInt();
+            mp.put(x,mp.getOrDefault(x,0)+ 1);
+        }
+        for(var i : mp.entrySet()){
+            System.out.println(i.getKey() + " " + i.getValue());
+        }
+    }
+}
