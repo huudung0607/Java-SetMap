584274cd18846adc1f3850a3fe4487849c58a0c5
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-09-11 14:29:48 +0700

    Create Set Map & int 2.java

---
+++ Set Map & int 2.java
@@ -0,0 +1,18 @@
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.*;
+public class Main {
+    private static int mod = 1000000007;
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        TreeSet<Integer> se = new TreeSet<>(Collections.reverseOrder());
+        for(int i = 0 ; i < n ;i++){
+            se.add(sc.nextInt());
+        }
+        System.out.println(se.size());
+        for(var i : se){
+            System.out.print(i + " ");
+        }
+    }
+}
